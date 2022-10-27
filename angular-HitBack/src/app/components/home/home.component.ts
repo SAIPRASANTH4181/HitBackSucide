992b56514faf817dab3035317135504972515f27
Varsha Reddy Banda <varshareddybanda@Varshas-Air.attlocal.net>
2022-10-26 23:26:03 -0500

    Modified code for the Web-Home pagein home.component.css and home.component.ts files

--- angular-HitBack/src/app/components/home/home.component.ts
+++ angular-HitBack/src/app/components/home/home.component.ts
@@ -12,5 +12,19 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  
+  ExcelData : any;
+
+
+ReadExcel(event:any)
+  {
+    let file = event.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onload = (e)=>{
+      var workBook = XLSX.read(fileReader.result,{type:'binary'});
+      var sheetNames = workBook.SheetNames;
+      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+      console.log(this.ExcelData);
+    }
+  }
 }
