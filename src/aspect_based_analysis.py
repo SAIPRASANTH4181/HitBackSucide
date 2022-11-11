54b1ce7020c6b079e1d960469a98223de43cd46b
Akhilandeshwari Ramu <akhilandeshwari.ramu@slu.edu>
2022-11-11 10:47:37 -0600

    Updated aspcted_based_analysis.py
    Added additional aspect features

--- src/aspect_based_analysis.py
+++ src/aspect_based_analysis.py
@@ -27,7 +27,7 @@ def aspect_based_analysis(toy_rev):
     sid = SentimentIntensityAnalyzer()
     
     # List of emotions that are used to analyze the sentiments
-    competitors = ['Depression','Stress','Relationship','Emotions','Finance','Society']
+    competitors = ['Depression','Stress','Relationship','Emotions','Finance','Society','Anxiety','Panic','Nervous','Annoying']
     aspect_terms = []
     comp_terms = []
     easpect_terms = []
