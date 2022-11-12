6eeeb9c98d6179d1e216e2f852524ddf49354b7e
prasanth4181 <saiprasanth.bodaballa@slu.edu>
2022-11-11 21:33:31 -0600

    updated a spec.ts file

--- angular-HitBack/src/app/components/home/home.component.spec.ts
+++ angular-HitBack/src/app/components/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 import { HomeComponent } from './home.component';
 
@@ -9,6 +11,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
+      imports:[HttpClientTestingModule,ToastrModule.forRoot()]
     })
     .compileComponents();
   });
--- angular-HitBack/src/app/services/file-upload.service.spec.ts
+++ angular-HitBack/src/app/services/file-upload.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FileUploadService } from './file-upload.service';
 
 describe('FileUploadService', () => {
   let service: FileUploadService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule,]
+    });
+    
     service = TestBed.inject(FileUploadService);
   });
 
