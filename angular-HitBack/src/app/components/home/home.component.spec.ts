a79107a77058f4b58ffcac2610af85b7f49ca211
prasanth4181 <saiprasanth.bodaballa@slu.edu>
2022-12-02 09:09:43 -0600

    added a test case

--- angular-HitBack/src/app/components/home/home.component.spec.ts
+++ angular-HitBack/src/app/components/home/home.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToastrModule } from 'ngx-toastr';
-
+import { By } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -25,4 +25,13 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('chcecking file change event in handler', () => {
+    let input  = fixture.debugElement.query(By.css('input[type=file]')).nativeElement;
+    spyOn(component, 'onFileChange');
+    input.dispatchEvent(new Event('change'));
+    expect(component.onFileChange).toHaveBeenCalled();
+  });
+
+
 });
